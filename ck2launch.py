664b1286ea24d8dccfa2ff1ec1da176faebc5497
Israel Halle <isra017@gmail.com>
2013-05-30 23:42:18 -0400

    Changed default ini with mods and dlcs

--- ck2launch.py
+++ ck2launch.py
@@ -43,6 +43,7 @@ def getArgs():
 
 def getOptions():
     config = getConfig()
+    print(config)
     parser.set_defaults(**{
         'mods': config['ck2'].get('mods',[]),
         'exclude_dlc': config['ck2'].get('exclude_dlc',[])
