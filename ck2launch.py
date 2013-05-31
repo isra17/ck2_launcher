0175d8383aad8abd4c5c5b43f7318eb27b033317
Israel Halle <isra017@gmail.com>
2013-05-30 22:35:13 -0400

    Launch ck2 with mods and dlc

--- ck2launch.py
+++ ck2launch.py
@@ -4,21 +4,34 @@ import argparse
 import sys
 import os
 
-CONFIG_PATH = os.path.expanduser('~/.ck2launcher')
+CONFIG_PATH = ['.ck2launcher', os.path.expanduser('~/.ck2launcher')]
 
 parser = argparse.ArgumentParser(description=
     'Launcher for Crusader King II that allow to select mods and DLC')
 parser.add_argument('-m', '--mods', nargs='*', help='Mods loaded')
-parser.add_argument('-d', '--dlc', nargs='*', help='DLCs loaded')
+parser.add_argument('-d', '--exclude-dlc', nargs='*', help='DLCs excluded')
 
 def getConfig():
     config = configparser.ConfigParser()
     config.read(CONFIG_PATH)
 
-    config_dict = {'ck2':{},'steam':{}}
+    config_dict = {
+        'ck2':{
+            'mods':[],
+            'exclude_dlc':[]
+        },
+        'steam':{}
+    }
 
-    for section in config.sections():
-        config_dict[section] = dict(config.items(section))
+    config_dict['steam'] = dict(config.items('steam'))
+    if config.has_option('ck2', 'exclude_dlc'):
+        config_dict['ck2']['exclude_dlc'] = \
+            config.get('ck2','exclude_dlc').split('\n')
+
+    if config.has_option('ck2', 'mods'):
+        config_dict['ck2']['mods'] = \
+            config.get('ck2','mods').split('\n')
+
 
     return config_dict
 
@@ -29,20 +42,30 @@ def getArgs():
 def getOptions():
     config = getConfig()
     parser.set_defaults(**{
-        'mods': config['ck2'].get('mods',''),
-        'dlc': config['ck2'].get('dlc','')
+        'mods': config['ck2'].get('mods',[]),
+        'exclude_dlc': config['ck2'].get('exclude_dlc',[])
     })
     args = getArgs()
 
     config['ck2']['mods'] = args.mods
-    config['ck2']['dlc'] = args.dlc
+    config['ck2']['exclude_dlc'] = args.exclude_dlc
 
     return config
 
-def launchCk2():
-    options = getOptions()
+def launchck2(options):
+    ck2_path = os.path.expanduser(options['steam']['ck2_bin'])
+    args = [ck2_path]
+    args.extend(list(map(lambda mod: '-mod=mod/' + mod,
+            options['ck2']['mods'])))
+    args.extend(list(map(lambda dlc: '-exculde_dlc=dlc/' + dlc,
+            options['ck2']['exclude_dlc'])))
 
+    os.execvp(ck2_path, args)
+
+def main():
+    options = getOptions()
+    launchck2(options)
 
 
 if __name__ == '__main__':
-    launchCk2()
+    main()
